89755bb885c1e8d7a2db8549bf40aca921fbbb32
GGxxxxxx <guoxingg3@sina.com>
2025-04-22 04:00:29 +0100

    Add files via upload

---
+++ src/build7/CMakeFiles/FACEqt.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FACEqt_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/FACEqt_autogen.dir/ParseCache.txt"
+  "FACEqt_autogen"
+  "CMakeFiles/FACEqt.dir/FACEqt_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/FACEqt.dir/FACEqt_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/FACEqt.dir/main.cpp.o"
+  "CMakeFiles/FACEqt.dir/main.cpp.o.d"
+  "CMakeFiles/FACEqt.dir/mainwindow.cpp.o"
+  "CMakeFiles/FACEqt.dir/mainwindow.cpp.o.d"
+  "FACEqt"
+  "FACEqt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FACEqt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/build7/FACEqt_autogen/EWIEGA46WW/moc_mainwindow.cpp
@@ -0,0 +1,136 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.8)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../../mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.8. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[10];
+    char stringdata0[95];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 15), // "toggleDetection"
+QT_MOC_LITERAL(2, 27, 0), // ""
+QT_MOC_LITERAL(3, 28, 17), // "handleCameraFrame"
+QT_MOC_LITERAL(4, 46, 7), // "cv::Mat"
+QT_MOC_LITERAL(5, 54, 5), // "frame"
+QT_MOC_LITERAL(6, 60, 9), // "updateBPM"
+QT_MOC_LITERAL(7, 70, 3), // "bpm"
+QT_MOC_LITERAL(8, 74, 14), // "appendIRSample"
+QT_MOC_LITERAL(9, 89, 5) // "value"
+
+    },
+    "MainWindow\0toggleDetection\0\0"
+    "handleCameraFrame\0cv::Mat\0frame\0"
+    "updateBPM\0bpm\0appendIRSample\0value"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       4,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   34,    2, 0x08 /* Private */,
+       3,    1,   35,    2, 0x08 /* Private */,
+       6,    1,   38,    2, 0x08 /* Private */,
+       8,    1,   41,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 4,    5,
+    QMetaType::Void, QMetaType::Float,    7,
+    QMetaType::Void, QMetaType::Int,    9,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->toggleDetection(); break;
+        case 1: _t->handleCameraFrame((*reinterpret_cast< cv::Mat(*)>(_a[1]))); break;
+        case 2: _t->updateBPM((*reinterpret_cast< float(*)>(_a[1]))); break;
+        case 3: _t->appendIRSample((*reinterpret_cast< int(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 4)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 4;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 4)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 4;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
